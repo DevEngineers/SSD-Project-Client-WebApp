fd9edfe65d308dc5f599956c6cc80936491809ae
nusky <it19167442@my.sliit.lk>
2022-11-02 21:57:34 +0530

    file upload page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login';
+import FileUpload from "./Pages/FileUpload";
 
 const App = () => {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Login />} />
+				<Route path="/fileUpload" element={<FileUpload />} />
 			</Routes>
 		</Router>
 	);
