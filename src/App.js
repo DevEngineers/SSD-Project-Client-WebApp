9e3df7d73d99e04ef2588b88f9fc463e66793956
Mohamed Akeel <akeel1447@gmail.com>
2022-11-02 15:59:57 +0530

    Adding keycloak authentication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import Keycloak from 'keycloak-js';
+import Home from './Pages/Home';
 import Login from './Pages/Login';
+import keycloakData from '../keycloak.json';
 
 const App = () => {
+	const keycloak = new Keycloak(keycloakData);
+
+	const initOptions = {
+		onLoad: 'login-required',
+		checkLoginIframe: false,
+	};
+
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Login />} />
-			</Routes>
-		</Router>
+		<ReactKeycloakProvider authClient={keycloak} initOptions={initOptions}>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</Router>
+		</ReactKeycloakProvider>
 	);
 };
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Keycloak from 'keycloak-js';
+import keycloakData from '../../keycloak.json';
+
+const Home = () => {
+	const [keycloak, setKeycloak] = useState(null);
+	const [authenticated, setAuthenticated] = useState(false);
+
+	useEffect(() => {
+		const keycloak = new Keycloak(keycloakData);
+		keycloak.init({ onLoad: 'login-required'})
+			.then(authenticated => {
+				setKeycloak(keycloak);
+				setAuthenticated(authenticated);
+			});
+	}, []);
+
+	if (keycloak) {
+		if (authenticated)
+			return (
+				<div className="my-12 grid place-items-center">
+					<p> You are logged in.</p>
+				</div>
+			);
+		else return <div className="my-12">Unable to initiate auth!</div>;
+	}
+
+	return (
+		<>
+			<div className="my-12">Keycloak initializing in a moment...</div>
+		</>
+	);
+};
+
+export default Home;
